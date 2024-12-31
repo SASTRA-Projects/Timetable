a506f13be61a01e3030bd4cffe19d072a3978298
Harikrishna-Srinivasan-1 <harikrishnasri3@gmail.com>
2024-12-31 09:33:35 +0530

    Created boilerplate codes

---
+++ relations.py
@@ -0,0 +1,33 @@
+import database
+
+
+database.cursor.execute("""CREATE TABLE IF NOT EXISTS `tutors` (
+						`faculty_id` SMALLINT UNSIGNED,
+						`class_id` SMALLINT UNSIGNED,
+						PRIMARY KEY(`faculty_id`, `class_id`),
+						FOREIGN KEY(`faculty_id`) REFERENCES `faculties`(`id`),
+						FOREIGN KEY(`class_id`) REFERENCES `classes`(`id`)
+)""")
+database.cursor.execute("""CREATE TABLE IF NOT EXISTS `faculty_teaches_class` (
+						`id` SMALLINT UNSIGNED AUTO_INCREMENT,
+						`faculty_id` SMALLINT UNSIGNED NOT NULL,
+						`class_id` SMALLINT UNSIGNED NOT NULL,
+						`subject_id` SMALLINT UNSIGNED NOT NULL,
+						PRIMARY KEY(`id`),
+						FOREIGN KEY(`faculty_id`) REFERENCES `faculties`(`id`),
+						FOREIGN KEY(`class_id`) REFERENCES `classes`(`id`),
+						FOREIGN KEY(`subject_id`) REFERENCES `subjects`(`id`),
+						UNIQUE(`faculty_id`, `class_id`, `subject_id`)
+)""")
+database.cursor.execute("""CREATE TABLE IF NOT EXISTS `student_attends_class` (
+						`student_id` INT UNSIGNED,
+						`subject_id` SMALLINT UNSIGNED, -- check for total credit (minimum), ignoring minor electives
+						`class_id` SMALLINT UNSIGNED,
+						PRIMARY KEY(`student_id`, `subject_id`, `class_id`),
+						FOREIGN KEY(`student_id`) REFERENCES `students`(`id`),
+						FOREIGN KEY(`subject_id`) REFERENCES `subjects`(`id`),
+						FOREIGN KEY(`class_id`) REFERENCES `classes`(`id`)
+)""")
+
+if __name__ == "__main__":
+	database.close()
---
+++ timetable.py
@@ -0,0 +1,19 @@
+from relations import database
+
+database.cursor.execute("""CREATE TABLE IF NOT EXISTS `period` (
+						`id` TINYINT UNSIGNED AUTO_INCREMENT,
+						`time` TIME NOT NULL,
+						PRIMARY KEY(`id`)
+)""")
+
+database.cursor.execute("""CREATE TABLE IF NOT EXISTS `timetable` (
+						`day` ENUM("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday") NOT NULL,
+						`period_id` TINYINT UNSIGNED NOT NULL,
+						`faculty_teaches_class_id` SMALLINT UNSIGNED NOT NULL,
+						PRIMARY KEY(`day`, `period_id`),
+						FOREIGN KEY(`faculty_teaches_class_id`) REFERENCES `faculty_teaches_class`(`id`),
+						FOREIGN KEY(`period_id`) REFERENCES `period`(`id`)
+)""")
+
+if __name__ == "__main__":
+	database.close()
