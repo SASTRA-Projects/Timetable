d187296a03adc88d8a43413d542e1d807a28eb61
Harikrishna Srinivasan <harikrishnasri3@gmail.com>
2025-01-08 18:34:21 +0530

    Update mysql_connector.py
    For privacy, changed password argument

--- mysql_connector.py
+++ mysql_connector.py
@@ -1,14 +1,14 @@
 from mysql.connector import connect, errors
-# from dotenv import load_dotenv
-# from os import getenv
+from dotenv import load_dotenv
+from os import getenv
 
-# load_dotenv()
+load_dotenv()
 
-# db_connector = connector.connect(
-# 	user="root", password=getenv("SASTRA_pwd"), host="localhost"
-# )
+db_connector = connector.connect(
+	user="root", password=getenv("SASTRA_pwd"), host="localhost"
+)
 
-db_connector = connect(user="root", password="password", host="localhost")
+# db_connector = connect(user="root", ?, host="localhost")
 if not db_connector.is_connected():
 	raise Exception("Database connection failed")
 
