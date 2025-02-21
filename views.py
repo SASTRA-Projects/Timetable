efa4d4e052042a4ad27f3ba5692e3b1abbef7e1c
Harikrishna Srinivasan <harikrishnasri3@gmail.com>
2025-02-21 22:03:18 +0530

    Bug: Flask did not expect bool return type

--- views.py
+++ views.py
@@ -1,8 +1,8 @@
 def create_views(db_connector, cursor):
-	import timetable
-	timetable.create_timetable(db_connector, cursor)
+    import timetable
+    timetable.create_timetable(db_connector, cursor)
 
-	cursor.execute("""CREATE OR REPLACE VIEW `faculty_view` AS
+    cursor.execute("""CREATE OR REPLACE VIEW `faculty_view` AS
                    SELECT * FROM `faculties`
                    JOIN `faculty_info`
                    ON `faculties`.`id`=`faculty_info`.`faculty_id`
@@ -15,4 +15,4 @@ def create_views(db_connector, cursor):
                    INNER JOIN `campuses`
                    ON `campuses`.`id`=`schools`.`campus_id`
     """)
-	db_connector.commit()
+    db_connector.commit()
