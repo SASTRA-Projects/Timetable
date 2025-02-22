96078a9c9750377a172947788dd1b13be0a5d39e
Harikrishna Srinivasan <harikrishnasri3@gmail.com>
2025-02-22 22:26:49 +0530

    Removed add, delete routes

--- fetch_data.py
+++ fetch_data.py
@@ -47,3 +47,115 @@
 # 					   		FROM `courses`
 # 						 	WHERE `id`=%s""", (course_id,))
 # 	return cursor.fetchone()
+
+from argon2 import PasswordHasher, exceptions
+
+def get_courses(cursor, /):
+    cursor.execute("""SELECT * FROM `courses`""")
+    return cursor.fetchall()
+
+def get_course(cursor, /, *, code=None):
+    cursor.execute("""SELECT * FROM `courses`
+                   WHERE `code`=%s""", (code,))
+    course = cursor.fetchone()
+    course.pop("code", None)
+    return course
+
+def get_elective_courses(cursor, /):
+    cursor.execute("""SELECT * FROM `courses`
+                   WHERE `is_elective`=%s""", (True,))
+    return cursor.fetchall()
+
+def get_nonelective_courses(cursor, /):
+    cursor.execute("""SELECT * FROM `courses`
+                   WHERE `is_elective`=%s""", (False,))
+    return cursor.fetchall()
+
+def get_courses_with_lab(cursor, /):
+    cursor.execute("""SELECT * FROM `courses`
+                   WHERE `P` > %s""", (0,))
+    return cursor.fetchall()
+
+def get_courses_without_lab(cursor, /):
+    cursor.execute("""SELECT * FROM `courses`
+                   WHERE `P`=%s""", (0,))
+    return cursor.fetchall()
+
+def get_classes(cursor, /):
+    cursor.execute("""SELECT * FROM `classes`""")
+    return cursor.fetchall()
+
+def get_class(cursor, /, building_id=None):
+    cursor.execute("""SELECT * FROM `classes`
+                   WHERE `building_id`=%s""", (building_id,))
+    classes = cursor.fetchall()
+    for _class in classes:
+        _class.pop("building_id", None)
+    return classes
+
+def get_lab_classes(cursor, /):
+    cursor.execute("""SELECT * FROM `classes`
+                   WHERE `is_lab`=%s""", (True,))
+    return cursor.fetchall()
+
+def get_non_lab_classes(cursor, /):
+    cursor.execute("""SELECT * FROM `classes`
+                   WHERE `is_lab`=%s""", (False,))
+    return cursor.fetchall()
+
+def get_faculties(cursor, /):
+	cursor.execute("""SELECT * FROM `faculties`""")
+	return cursor.fetchall()
+
+def get_faculty(cursor, /, *, id=None):
+	cursor.execute("""SELECT `name` FROM `faculties`
+				   WHERE `id`=%s""", (id,))
+	return cursor.fetchone()
+
+def get_students(cursor, /):
+	cursor.execute("""SELECT * FROM `students`""")
+	return cursor.fetchall()
+
+def get_student(cursor, /, *, id=None):
+	cursor.execute("""SELECT `name` FROM `students`
+				   WHERE `id`=%s""", (id,))
+	return cursor.fetchone()
+
+def get_student_by_info(cursor, /, *,
+                        campus_id=None,
+                        join_year=None,
+                        programme_id=None,
+                        roll_no=None):
+	cursor.execute("""SELECT `name` FROM `students`
+				   WHERE `campus_id`=%s
+                   AND `join_id`=%s
+                   AND `programme_id`=%s
+                   AND `roll_no`=%s""",
+                   (campus_id, join_year, programme_id, roll_no))
+	return cursor.fetchone()
+
+def get_faculty_details(cursor, /, *, id=None, password=None):
+	try:
+		cursor.execute("""SELECT * FROM `faculty_view`
+					   WHERE `id`=%s""", (id,))
+		faculty = cursor.fetchone()
+		ph = PasswordHasher()
+		ph.verify(faculty["password"], password)
+	except exceptions.VerifyMismatchError:
+		if ph.check_needs_rehash(faculty["password"]):
+			cursor.execute("""UPDATE `faculty_info`
+						   SET `password`=%s""", ph.hash(password))
+		del faculty["password"]
+		del ph
+		raise AssertionError("Incorrect Password")
+	del faculty["password"]
+	del password
+	del ph
+	return faculty
+
+def get_student_details(cursor, /, *, 
+                        campus_id=None,
+                        join_year=None,
+                        programme_id=None,
+                        roll_no=None):
+    pass
