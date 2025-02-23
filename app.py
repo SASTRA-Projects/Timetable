ea1a2ddd8e0fdc925c9a7c923cf0bde7fe4ce0fc
Harikrishna Srinivasan <harikrishnasri3@gmail.com>
2025-02-23 21:33:23 +0530

    Handled 404 & improved CSS

--- app.py
+++ app.py
@@ -98,5 +98,9 @@ def faculty_details():
 	faculty = session["faculty_details"]
 	return render_template("faculty.html", faculty=faculty, campus=show_data.get_campus_name(sql.cursor, id=faculty["campus_id"]))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return (render_template("404.html"), 404)
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=False)
