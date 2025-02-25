b9cecbb50c870ed1733f6829724dc5a6e963301e
Harikrishna Srinivasan <harikrishnasri3@gmail.com>
2025-02-25 09:06:18 +0530

    ERDiagram Added

--- insert_data.py
+++ insert_data.py
@@ -1,32 +1,4 @@
 from argon2 import PasswordHasher
-import datetime
-
-def insert_faculty_basic_info(db_connector,
-							  cursor, /, *,
-							  id=None,
-							  name=None,
-							  campus_id=None,
-							  department=None,
-							  join_year=datetime.date.today().year,
-							  phone=None,):
-	cursor.execute("""INSERT INTO `faculties`
-				   VALUES(%s, %s, %s, %s, %s, %s)""",
-				   (id, name, campus_id, department, join_year, phone))
-	db_connector.commit()
-
-def insert_student_basic_info(db_connector,
-							  cursor, /, *,
-							  id=None,
-							  name=None,
-							  campus_id=None,
-							  join_year=datetime.date.today().year,
-							  programme_id=None,
-							  roll_no=None,
-							  phone=None):
-	cursor.execute("""INSERT INTO `students`
-				   VALUES(%s, %s, %s, %s, %s, %s, %s)""",
-				   (id, name, campus_id, join_year, programme_id, roll_no, phone))
-	db_connector.commit()
 
 def insert_timetable(db_connector, cursor, /, *, day=None, period_id=None, faculty_teaches_class_id=None):
 	cursor.execute("""INSERT INTO `timetables`
--- mysql_connector.py
+++ mysql_connector.py
@@ -5,7 +5,7 @@ timeout = 300
 db_connector = None
 cursor = None
 
-def connect(user, __pwd, host="mysql-93e938b-harikrishnasri3.f.aivencloud.com"):
+def connect(user, __pwd, /, *, host="mysql-93e938b-harikrishnasri3.f.aivencloud.com"):
   global db_connector, cursor
   db_connector = pymysql.connect(
     charset="utf8mb4",
@@ -20,12 +20,14 @@ def connect(user, __pwd, host="mysql-93e938b-harikrishnasri3.f.aivencloud.com"):
   )
 
   cursor = db_connector.cursor()
-  db_connector.autocommit = False
-
-  cursor.execute("""CREATE DATABASE IF NOT EXISTS `SASTRA`""")
-  cursor.execute("""USE `SASTRA`""")
-
   return db_connector
+
+def create_db(*, database=None):
+  cursor.execute("""CREATE DATABASE IF NOT EXISTS `%s`""", (database,))
+  
+def use_db(*, database=None):
+  db_connector.autocommit = False
+  cursor.execute("""USE `%s`""", (database,))
 
 def close():
 	db_connector.commit()
