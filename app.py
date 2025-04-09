f06c8d9e58c481e068d82e1038a3952481795272
Harikrishna Srinivasan <harikrishnasri3@gmail.com>
2025-04-09 09:11:34 +0530

    Removed a blank line: For Workflow testing

--- app.py
+++ app.py
@@ -147,6 +147,5 @@ if __name__ == "__main__":
 	app.config['SESSION_COOKIE_HTTPONLY'] = True
 	app.config['SESSION_COOKIE_SECURE'] = True
 
-
 	app.config.update(SESSION_COOKIE_SECURE=True, SESSION_COOKIE_HTTPSONLY=True)
 	app.run(host="0.0.0.0", port=5000, debug=False)
